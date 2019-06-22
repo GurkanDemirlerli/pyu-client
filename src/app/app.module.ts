0cde2b4d625072e491436ff3e27411023c04253c
GurkanDemirlerli <gurkandemirlerli@gmail.com>
2019-06-22 21:59:32 +0300

    commit fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
   declarations: [
     AppComponent,
     AddTaskModalComponent
+    
   ],
   imports: [
     BrowserModule,
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
   production: true
 };
+
+export const server = {
+  url: "http://localhost:3000"
+}
